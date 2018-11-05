725ae4207c2304d1c9bb8eb2f9679ec289e29780
Assane Gueye <agueye@lexus.cam.nist.gov>
2018-11-05 11:02:00 -0500

    test

---
+++ buid_graphs.py
@@ -0,0 +1,15 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+num_nodes=4
+base='../Data/'
+in_file=base+"rdag"+str(4)+".txt"
+
+with open(in_file,"r") as f:
+    line=f.readline()
+    count=1
+    while line:
+        count+=1
+        links=list(line)
+        print(links)
