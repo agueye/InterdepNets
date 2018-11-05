1f4210be1ee2e2d0b065ce092a8129cf1446aacf
agueye <assane.gueye@gmail.com>
2018-11-05 10:17:29 -0500

    First commit

---
+++ buid_graphs.py
@@ -0,0 +1,16 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+num_nodes=4
+base='../Data/'
+in_file=base+"rdag"+str(4)+".txt"
+
+with open(in_file,"r") as f:
+    line=f.readline()
+    line=line.strip('\n')
+    count=1
+    while line:
+        count+=1
+        links=list(line)
+        print(links)
